f51d93a64d3f617cfd6665030d141c49f832f8b6
rehan-bhatia <bhatia.rehan4@icloud.com>
2020-11-15 22:30:16 +0530

    improving mobile view

--- src/components/leaderboard.js
+++ src/components/leaderboard.js
@@ -42,7 +42,7 @@ const Leaderboard = ({data}) => {
         <div>
           <div style={{display: "flex", flexWrap: "wrap", justifyContent: "flex-start"}}>
             <img src = {data.url} class = "pa2 br-100 ba b--black-10" style={{height:"70px", width:"70px"}} />
-            <div class = "ml3">
+            <div class = "ml3 w-20">
               <p class="f4 pt2 ph2 mb0">{data.name}</p>
               <p class="f5 pa2">{data.points} points</p>
             </div>
