486ee0637c85c84d026fa609967cb0c480581594
Om Krishna <omkrishna.ok@gmail.com>
2020-11-17 16:16:43 +0530

    started on homepage design

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,7 @@ const Nav = () => {
 		navigate(route)
 	}
 	return(
-		<nav style={{fontFamily: "Avenir"}} class="navbar db dt-l w-100 border-box bg-black pa3 ph5-l pv0">
+		<nav style={{fontFamily: "garamond"}} class="navbar db dt-l w-100 border-box bg-black pa3 ph5-l pv0">
 		  <p class="dib dtc-l v-mid white w-100 w-25-l tc tl-l  mb0-l mb2"><h3 class="nav mt0">QC IITD</h3></p>
 		  <div class="db dtc-l pa2v-mid w-100 w-75-l tc tr-l mb0-l mt2 mb2 pt2">
 		  	<p onClick={()=>onClick("/")} class="nav pointer mt link white dib mr3 mt3 mr4-l hover-green">Home</p>
