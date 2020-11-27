eab73c7103373d650a737a97c00fb9950667838f
rehan-bhatia <bhatia.rehan4@icloud.com>
2020-11-27 16:11:23 +0530

    fixed mobile view on calendar

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "tachyons"
-import {isBrowser} from "react-device-detect"
+import {isBrowser, BrowserView, MobileView} from "react-device-detect"
 
 import Nav from "../components/Nav.js";
 import "../components/style.css"
@@ -24,22 +24,33 @@ class Calendar extends React.Component {
 	render() {
 		if (this.state.route == "current") {
 			output = (
-				<div class="flex items-center justify-center ph4 pt0">
-					<p onClick = {() => this.onRouteChange("old")} class="f5 no-underline pointer br-100 black bg-animate hover-bg-green hover-white inline-flex items-center pa3 ba border-box mr4">
-						<svg class="w1 hover" data-icon="chevronLeft" viewBox={(isBrowser) ? "0 0 32 32" : "0 0 16 16"}>
-							<title>chevronLeft icon</title>
-							<path d="M20 1 L24 5 L14 16 L24 27 L20 31 L6 16 z"></path>
-						</svg>
-						<span class="pl1"></span>
-					</p>
-					<p class={`pa2 mr4 ${(isBrowser) ? "f2" : "f3"} avenir green`}>Events this Month</p>
-					<p onClick = {() => this.onRouteChange("new")} class="f5 no-underline pointer black bg-animate pointer hover-bg-green br-100 hover-white inline-flex items-center pa3 ba border-box">
-					    <span class="pr1"></span>
-					    <svg class="w1" data-icon="chevronRight" viewBox={(isBrowser) ? "0 0 32 32" : "0 0 16 16"}>
-					      <title>chevronRight icon</title>
-					      <path d="M12 1 L26 16 L12 31 L8 27 L18 16 L8 5 z"></path>
-					    </svg>
-					</p>
+				<div>
+					<BrowserView>
+						<div class="flex items-center justify-center ph4 pt0">
+							<p onClick = {() => this.onRouteChange("old")} class="f5 no-underline pointer br-100 black bg-animate hover-bg-green hover-white inline-flex items-center pa3 ba border-box mr4">
+								<svg class="w1 hover" data-icon="chevronLeft" viewBox="0 0 32 32">
+									<title>chevronLeft icon</title>
+									<path d="M20 1 L24 5 L14 16 L24 27 L20 31 L6 16 z"></path>
+								</svg>
+								<span class="pl1"></span>
+							</p>
+							<p class={`pa2 mr4 ${(isBrowser) ? "f2" : "f3"} avenir green`}>Events this Month</p>
+							<p onClick = {() => this.onRouteChange("new")} class="f5 no-underline pointer black bg-animate pointer hover-bg-green br-100 hover-white inline-flex items-center pa3 ba border-box">
+							    <span class="pr1"></span>
+							    <svg class="w1" data-icon="chevronRight" viewBox="0 0 32 32">
+							      <title>chevronRight icon</title>
+							      <path d="M12 1 L26 16 L12 31 L8 27 L18 16 L8 5 z"></path>
+							    </svg>
+							</p>
+						</div>
+					</BrowserView>
+					<MobileView>
+						<div class="flex items-center justify-center ph2 pt0">
+							<p onClick = {() => this.onRouteChange("old")} class="pv3 ph3 pointer br-100 black bg-animate hover-bg-green hover-white">{`<`}</p>
+							<p class="f3 pa2 tc">Events this Month</p>
+							<p onClick = {() => this.onRouteChange("new")} class="pv3 ph3 pointer br-100 black bg-animate hover-bg-green hover-white">{`>`}</p>
+						</div>
+					</MobileView>
 				</div>
 			)
 			poster_group = (
@@ -56,15 +67,25 @@ class Calendar extends React.Component {
 
 		} else if (this.state.route == "new") {
 			output = (
-				<div class="flex items-center justify-center ph4 pt0">
-					<p onClick = {() => this.onRouteChange("current")} class="f5 no-underline pointer br-100 black bg-animate hover-bg-green hover-white inline-flex items-center pa3 ba border-box mr4">
-						<svg class="w1 hover" data-icon="chevronLeft" viewBox={(isBrowser) ? "0 0 32 32" : "0 0 16 16"}>
-							<title>chevronLeft icon</title>
-							<path d="M20 1 L24 5 L14 16 L24 27 L20 31 L6 16 z"></path>
-						</svg>
-						<span class="pl1"></span>
-					</p>
-					<p class={`pa2 mr4 ${(isBrowser) ? "f2" : "f3"} avenir green`}>Upcoming Events</p>
+				<div>
+					<BrowserView>
+						<div class="flex items-center justify-center ph4 pt0">
+							<p onClick = {() => this.onRouteChange("current")} class="f5 no-underline pointer br-100 black bg-animate hover-bg-green hover-white inline-flex items-center pa3 ba border-box mr4">
+								<svg class="w1 hover" data-icon="chevronLeft" viewBox="0 0 32 32">
+									<title>chevronLeft icon</title>
+									<path d="M20 1 L24 5 L14 16 L24 27 L20 31 L6 16 z"></path>
+								</svg>
+								<span class="pl1"></span>
+							</p>
+							<p class={`pa2 mr4 ${(isBrowser) ? "f2" : "f3"} avenir green`}>Upcoming Events</p>
+						</div>
+					</BrowserView>
+					<MobileView>
+						<div class="flex items-center justify-center ph2 pt0">
+							<p onClick = {() => this.onRouteChange("current")} class="pv3 ph3 pointer br-100 black bg-animate hover-bg-green hover-white">{`<`}</p>
+							<p class="f3 pa2 tc">Upcoming Events</p>
+						</div>
+					</MobileView>
 				</div>
 			)
 			poster_group = (
@@ -80,16 +101,27 @@ class Calendar extends React.Component {
 
 		} else if (this.state.route == "old") {
 			output = (
-				<div class="flex items-center justify-center ph4 pt0">
-					<p class={`pa2 mr4 ${(isBrowser) ? "f2" : "f3"} avenir green`}>Past Events</p>
-					<p onClick = {() => this.onRouteChange("current")} class="f5 no-underline pointer black bg-animate pointer hover-bg-green br-100 hover-white inline-flex items-center pa3 ba border-box">
-					    <span class="pr1"></span>
-					    <svg class="w1" data-icon="chevronRight" viewBox={(isBrowser) ? "0 0 32 32" : "0 0 16 16"}>
-					      <title>chevronRight icon</title>
-					      <path d="M12 1 L26 16 L12 31 L8 27 L18 16 L8 5 z"></path>
-					    </svg>
-					</p>
+				<div>
+					<BrowserView>
+						<div class="flex items-center justify-center ph4 pt0">
+							<p class={`pa2 mr4 ${(isBrowser) ? "f2" : "f3"} avenir green`}>Past Events</p>
+							<p onClick = {() => this.onRouteChange("current")} class="f5 no-underline pointer black bg-animate pointer hover-bg-green br-100 hover-white inline-flex items-center pa3 ba border-box">
+							    <span class="pr1"></span>
+							    <svg class="w1" data-icon="chevronRight" viewBox="0 0 32 32">
+							      <title>chevronRight icon</title>
+							      <path d="M12 1 L26 16 L12 31 L8 27 L18 16 L8 5 z"></path>
+							    </svg>
+							</p>
+						</div>
+					</BrowserView>
+					<MobileView>
+						<div class="flex items-center justify-center ph2 pt0">
+							<p class="f3 pa2 tc">Past Events</p>
+							<p onClick = {() => this.onRouteChange("current")} class="pv3 ph3 pointer br-100 black bg-animate hover-bg-green hover-white">{`>`}</p>
+						</div>
+					</MobileView>
 				</div>
+
 			)
 
 			poster_group = (
